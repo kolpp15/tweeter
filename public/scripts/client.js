bcd6b877feab015c4a89319eb798f834e0f7dc82
Brian Sohn <kolpp15@gmail.com>
2021-08-11 18:06:34 -0700

    added rendering tweet, added initial data, deleted html tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 $(document).ready(function() {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
   
-  const initData = {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1628546254537
-};
+  // iterating through initial data and rendering
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $newTweet = createTweetElement(tweet);
+      $('#tweets-container').append($newTweet);
+    }
+  };
 
+  // appending new tweets function
   const createTweetElement = function(tweetData) {
-
     const $newTweet = `
     <article class="tweet">
     <header class="tweet">
@@ -43,13 +65,10 @@ $(document).ready(function() {
       </div>
     </footer>
 
-  </article>
+    </article>
     `;
     return $newTweet;
   };
 
-  const $newTweet = createTweetElement(initData);
-
-  $('#tweets-container').append($newTweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 });
